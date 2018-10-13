f2e9aa70cadb91d4385d1135f984d953d138c5a8
Andy Baugh <andy@troglodyne.net>
2018-10-13 10:35:03 -0500

    Implement new login, Improve installer script

--- README.md
+++ README.md
@@ -10,17 +10,19 @@ Oh, yeah, did I mention it's responsive by default and degrades well on IE (last
 
 See http://tcms.troglodyne.net for more information.
 
-Installing this is pretty easy,
-either grab an archive from above site and extract it or git clone it into a public html directory.
+INSTALLING
+----------
+* Clone this repo to wherever.
+* Run `bin/install` from the repository's top directory. It will guide you through the process.
+* Load the website you installed this to in a web browser. Continue to follow instructions.
 
-WARNING: If you don't setup HTTP Server based Authentication, you deserve what you get.
-See the manual for more information: http://tcms.troglodyne.net/index.php?nav=1&dir=fileshare/manual
-
-As of the latest version, there should be no upgrade issues,
-despite switching to using JSON to store new postings.
-The code anticipates and uses the legacy style of accessing old posts in that instance.
+UPGRADING
+---------
+* Re-run the installer. Since you have already installed, it will skip most prompting and
+simply update the files.
 
 TODO/Ideas:
+-----------
  * Convert blog posts to use JSON, similar to microblog, mostly to enable storing better metadata.
  * Theming importation ability, or a decent upgrading script for more effective cruise control.
  * Test code. I'll probably do this in perl,
--- sys/admin/index.php
+++ sys/admin/index.php
@@ -1,13 +1,27 @@
 <?php
-    include_once("lib/auth.inc");
-	auth::ensure_auth();
-    if( empty($_GET['app']) || $_GET['app'] == 'config' ) {
+    $args = ( $_SERVER['REQUEST_METHOD'] == 'POST' ? $_POST : $_GET );
+    $user_info = posix_getpwuid(posix_geteuid());
+    // Probably the 'sanest' default I could think of when you have no homedir
+    $dir = ( $user_info['dir'] ? $user_info['dir'] : '/var/www/' );
+    $basedir = ( file_exists( $dir . "/.tCMS_basedir") ? file_get_contents("$dir/.tCMS_basedir") : "$dir/.tCMS" );
+    if( !empty($args['app']) && $args['app'] == 'login' ) {
+        include("login.inc");
+        die();
+    } elseif( !empty($args['app']) && $args['app'] == 'logout' ) {
+        include("logout.inc");
+        die();
+    } else {
+        include_once("$basedir/lib/auth.inc");
+        $auth = new auth;
+        $auth->ensure_auth();
+    }
+    if( empty($args['app']) || $args['app'] == 'config' ) {
         $kontent = "settings.inc";
-    } elseif ($_GET['app'] == 'blog') {
+    } elseif ($args['app'] == 'blog') {
         $kontent = "bengine.inc";
-    } elseif ($_GET['app'] == 'microblog') {
+    } elseif ($args['app'] == 'microblog') {
         $kontent = "mbengine.inc";
-    } elseif ($_GET['app'] == 'users' ) {
+    } elseif ($args['app'] == 'users' ) {
         $kontent = "users.inc";
     } else {
         $kontent = "settings.inc";
@@ -32,16 +46,17 @@
   <link rel="icon" type="image/vnd.microsoft.icon" href="../../img/icon/favicon.ico" />
   <title>tCMS Admin</title>
   <?php
-   $config = json_decode(file_get_contents('config/main.json'),true);
+   $config = @json_decode(@file_get_contents("$basedir/config/main.json"),true);
   ?>
  </head>
  <body>
   <div id="topkek" style="text-align: center; vertical-align: middle;">
    <button title="Menu" id="clickme">&#9776;</button>
    <span id="configbar">
-    <a class="topbar" title="Edit Various Settings" href="index.php">Settings</a>
-    <a class="topbar" title="Blog Writer" href="index.php?nav=1">Blog Writer</a>
-    <a class="topbar" title="Pop off about Stuff" href="index.php?nav=2">MicroBlogger</a>
+    <a class="topbar" title="Edit Various Settings" href="index.php?app=config">Settings</a>
+    <a class="topbar" title="Blog Writer" href="index.php?app=blog">Blog Writer</a>
+    <a class="topbar" title="Pop off about Stuff" href="index.php?app=microblog">MicroBlogger</a>
+    <a class="topbar" title="Logout" href="index.php?app=logout">Logout</a>
    </span>
   </div>
   <div id="kontent" style="display: block;">
