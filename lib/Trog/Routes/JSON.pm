51ed754e0fee3bcbcda1623c8bdf7a3cc65f0845
Andy Baugh <andy@troglodyne.net>
2022-01-04 16:46:25 +0000

    Add more comprehensive favicon handling
    Issue #141: Adds several favicon handling methods, as apparently
    every smartyphone and windows wants to do everything their own way.
    Note that I am specifically not adding the old .ico files, as
    the only things that really look specifically for /favicon.ico
    usually:
    1) Would ignore the themed dir anyways
    2) Probably would be broken due to expecting some size the icon did
       not even support/downscale well to.
    
    A helper script has been added to bin/ to create the various crap pngs
    that apple/android wants based on an input SVG file (which is probably
    what you should be using for icons nowadays).

--- lib/Trog/Routes/JSON.pm
+++ lib/Trog/Routes/JSON.pm
@@ -14,6 +14,11 @@ our %routes = (
         callback   => \&catalog,
         parameters => [],
     },
+    '/api/webmanifest' => {
+        method         => 'GET',
+        callback       => \&webmanifest,
+        parameters     => [],
+    },
 );
 
 my $contenttype = "Content-type:application/json;";
@@ -25,6 +30,18 @@ sub catalog ($query, $input, $=) {
         delete $rcopy{$r}{callback}
     }
     return [200,[$contenttype],[$enc->encode(\%rcopy)]];
+}
+
+sub webmanifest ($query, $input, $=) {
+    my $enc = JSON::MaybeXS->new( utf8 => 1 );
+    my %manifest = (
+        "icons" => [
+            { "src" => "$query->{theme_dir}/img/icon/favicon-192.png", "type" => "image/png", "sizes" => "192x192" },
+            { "src" => "$query->{theme_dir}/img/icon/favicon-512.png", "type" => "image/png", "sizes" => "512x512" },
+        ],
+    );
+
+    return [ 200, [$contenttype], [$enc->encode(\%manifest)] ];
 }
 
 1;
