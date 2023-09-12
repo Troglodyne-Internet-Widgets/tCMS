d4d8bacae2911c429ce2ae2e7bfb4126a2aeaab4
George Baugh <george@troglodyne.net>
2023-09-12 15:40:03 +0000

    de-stomp earlier vars (oops)

--- lib/Trog/Vars.pm
+++ lib/Trog/Vars.pm
@@ -6,3 +6,16 @@ use warnings;
 #1MB chunks
 our $CHUNK_SEP  = 'tCMSep666YOLO42069';
 our $CHUNK_SIZE = 1024000;
+
+our %content_types = (
+    plain => "text/plain;",
+    html  => "text/html; charset=UTF-8",
+    json  => "application/json;",
+    blob  => "application/octet-stream;",
+);
+
+our %cache_control = (
+    revalidate => "no-cache, max-age=0",
+    nocache    => "no-store",
+    static     => "public, max-age=604800, immutable",
+);
